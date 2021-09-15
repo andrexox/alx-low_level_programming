b8c2ae500e9cc3fbc4814a46d10aadf6df186e54
andrexox <ogundeleifeoluwa5@gmail.com>
2021-09-15 04:16:44 -0700

    Script 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,39 @@
 #include "main.h"
 
-int actual_prime(int n, int i);
-
 /**
- * is_prime_number - says if an integer is a prime number or not
- * @n: number to evaluate
+ * is_prime - recursively divide by higher divisor, skip even nums
+ * @n: number to check if prime
+ * @d: divisor
  *
- * Return: 1 if n is a prime number, 0 if not
+ * Return: 1 if prime, 0 if not, or recursive function call
  */
-int is_prime_number(int n)
+
+int is_prime(int n, int d)
 {
-	if (n <= 1)
+	if (n == d)
+		return (1);
+	if (n % d == 0)
 		return (0);
-	return (actual_prime(n, n - 1));
+	return (is_prime(n, d + 1));
+
 }
 
 /**
- * actual_prime - calculates if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
+ * is_prime_number - returns 1 if the input integer is a
+ * prime number, otherwise return 0.
+ * @n: number to check
  *
- * Return: 1 if n is prime, 0 if not
+ * Return: 1 if prime, 0 if not
  */
-int actual_prime(int n, int i)
+
+int is_prime_number(int n)
 {
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
+	int d = 3;
+
+	if (n % 2 == 0 || n < 2)
 		return (0);
-	return (actual_prime(n, i - 1));
+	if (n == 2)
+		return (1);
+
+	return (is_prime(n, d));
 }
