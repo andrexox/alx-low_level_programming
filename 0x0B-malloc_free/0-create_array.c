d4810411a3d60a9e23bc69d691c399975c99078f
andrexox <ogundeleifeoluwa5@gmail.com>
2021-09-21 04:30:11 -0700

    Script 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates array of chars and initializes it with a specific char
+ * create_array - creates array of chars and
+ * initializes it with a specific char
  * @size: size of array
  * @c: fill array values with this char
  *
